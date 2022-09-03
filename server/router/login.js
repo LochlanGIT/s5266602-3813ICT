5de5bb5b13d262a7371b871b1ae4a26b06afbabf
Lochlan Williams <lochlan.williams@griffithuni.edu.au>
2022-09-04 03:32:59 +1000

    basic login check

--- server/router/login.js
+++ server/router/login.js
@@ -3,19 +3,20 @@ var fs = require("fs");
 module.exports = function(req, res) {
   let valid = false;
   var u = req.body.username;
-  var p = req.body.pwd;
-  c = u + p;
+
   fs.readFile("./data/users.json", "utf8", function(err, data) {
     let userArray = JSON.parse(data);
 
     for (let i = 0; i < userArray.length; i++) {
-      if (u === userArray[i].username && p === userArray[i].pwd) {
+      if (u === userArray[i].username) {
         res.send({"ok": true});
         valid = true;
+        console.log("ACCEPTED")
         break;
       }
     }
     if (!valid) {
+      console.log("DENIED")
       res.send({"ok":false});
     }
 
