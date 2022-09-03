ec0e880f6ae247e71abaafb9383ae09b0bbc8771
Lochlan Williams <lochlan.williams@griffithuni.edu.au>
2022-09-04 03:48:53 +1000

    working on login functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ChatComponent } from './chat/chat.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,10 +13,12 @@ import { ChatComponent } from './chat/chat.component';
     LoginComponent,
     ChatComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+}
+import { AppComponent} from "../app.component";
+
+const BACKEND_URL = 'http://localhost:3000';
+
 
 @Component({
+  providers: [AppComponent],
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username = "";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, private httpClient: HttpClient, private comp: AppComponent) {}
+  ngOnInit() {}
+  public loginfunc() {
+    this.httpClient.post(BACKEND_URL + '/login', this.username, httpOptions)
+      .subscribe((data: any) => {
+        if (data.ok) {
+          // sessionStorage.setItem('username', this.userpwd.username);
+          // sessionStorage.setItem('userbirthdate', this.userobj.userbirthdate);
+          // sessionStorage.setItem('userage', (2022-this.userobj.userbirthdate.slice(0, 4)).toString());
+          this.router.navigateByUrl('chat');
+        } else {
+          alert('Sorry, username or password is not valid');
+        }
+      });
   }
-
 }
